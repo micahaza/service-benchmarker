016c1ed471f881bc9b1cf719d3c5137f23171e55
Pető Zoltán <bajgli@gmail.com>
2019-09-16 15:02:20 +0200

    service benchmarker with threads

--- service_benchmarker.py
+++ service_benchmarker.py
@@ -1,102 +1,27 @@
-
+import concurrent.futures
+import time
 import random
 import requests
-import time
+from test_input import config, fake_data
 
-config = {
-    "base_url": "http://localhost/",
-    "file_name": "output.log",
-    "requests_per_second": 100,
-    "test_lenght_in_seconds": 600
-}
+def do_work(item):
 
-urls = [
-    '/admin/',
-    '/auth/login',
-    '/auth/logout',
-    '/ping',
-    '/admin/users/',
-    '/admin/users/12',
-    '/admin/users/12/addresses'
-]
-
-headers = [
-    {'Content-Type': 'text/html' , 'charset': 'UTF-8'},
-    {'Content-Type': 'application/json'},
-    {'Content-Type': 'application/x-www-form-urlencoded'}
-]
-
-params = [
-    {},
-    {'username': 'joe', 'password': 'mypass'},
-    {'username': 'joe', 'password': 'mypass', 'email': 'joe@boo.com'},
-    {'message': 'hi there', 'to': 'goo@lala.hu', 'subject': 'Welcome'}
-]
-
-methods = ['POST', 'GET', 'DELETE', 'PUT', 'PATCH', 'OPTIONS']
+    # faking real world request distribution
+    time.sleep(random.randint(0, 1000) / 1000)
+    
+    if item['method'] == 'GET':
+        return requests.get(item['url'], headers=item['header'], params=item['param'])
+    elif item['method'] == 'POST':
+        return requests.post(item['url'], headers=item['header'], data=item['param'])
+    elif item['method'] == 'PUT':
+        return requests.put(item['url'], headers=item['header'], data=item['param'])
+    elif item['method'] == 'DELETE':
+        return requests.delete(item['url'], headers=item['header'], data=item['param'])
+    elif item['method'] == 'PATCH':
+        return requests.patch(item['url'], headers=item['header'], data=item['param'])
+    elif item['method'] == 'OPTIONS':
+        return requests.options(item['url'], headers=item['header'], data=item['param'])
 
 
-class ServiceBenchmarker(object):
-
-    def __init__(self):
-        self.tests = []
-        self.responses = []
-        self.base_url = "http://localhost:5000"
-        self.request_per_second = 10
-        self.test_lenght_in_seconds = 10
-        self.start_time = None
-        self.end_time = None
-
-    def init_test(self):
-        for x in range(self.request_per_second * self.test_lenght_in_seconds):
-            self.tests.append({
-                'method': methods[random.randint(0, len(methods) - 1)],
-                'url': self.base_url + urls[random.randint(0, len(urls) - 1)],
-                'header': headers[random.randint(0, len(headers) - 1)],
-                'param': params[random.randint(0, len(params) - 1)]
-                }
-            )
-
-    def run_all_tests(self):
-        self.start_time = time.time()
-        for t in self.tests:
-            req_start_time = time.time()
-            response = self.service_call(t)
-            req_end_time = time.time()
-            self.responses.append([response, req_end_time - req_start_time])
-        self.end_time = time.time()
-        
-    def service_call(self, test_case):
-        if test_case['method'] == 'GET':
-            return requests.get(test_case['url'], headers=test_case['header'], params=test_case['param'])
-        elif test_case['method'] == 'POST':
-            return requests.post(test_case['url'], headers=test_case['header'], data=test_case['param'])
-        elif test_case['method'] == 'PUT':
-            return requests.put(test_case['url'], headers=test_case['header'], data=test_case['param'])
-        elif test_case['method'] == 'DELETE':
-            return requests.delete(test_case['url'], headers=test_case['header'], data=test_case['param'])
-        elif test_case['method'] == 'PATCH':
-            return requests.patch(test_case['url'], headers=test_case['header'], data=test_case['param'])
-        elif test_case['method'] == 'OPTIONS':
-            return requests.options(test_case['url'], headers=test_case['header'], data=test_case['param'])
-
-    def report(self):
-        # number of requests
-        print('{} requests in {} seconds'.format(len(self.tests), self.end_time - self.start_time))
-        
-        # average request response time
-        s = sum(t[1] for t in self.responses)
-        print('average response time: {}'.format(s / len(self.tests)))
-
-        # number of successful requests
-        n = len([t[0] for t in self.responses if t[0].status_code in [200, 201, 204]])
-        print('number of successful requests: {}'.format(n)) 
-        
-        # number failed requests
-        n = len([t[0] for t in self.responses if t[0].status_code in [404, 500]])
-        print('number of failed requests: {}'.format(n)) 
-                
-sb = ServiceBenchmarker()
-sb.init_test()
-sb.run_all_tests()
-sb.report()
+with concurrent.futures.ThreadPoolExecutor(max_workers=config['requests_per_second']) as executor:
+        executor.map(do_work, fake_data())
---
+++ test_input.py
@@ -0,0 +1,41 @@
+import random
+
+
+config = {
+    "base_url": "http://localhost:5000",
+    "file_name": "output.log",
+    "requests_per_second": 10,
+    "test_lenght_in_seconds": 6
+}
+
+urls = [
+    '/admin/',
+    '/auth/login',
+    '/auth/logout',
+    '/ping',
+    '/admin/users/',
+    '/admin/users/12',
+    '/admin/users/12/addresses'
+]
+
+headers = [
+    {'Content-Type': 'text/html' , 'charset': 'UTF-8'},
+    {'Content-Type': 'application/json'},
+    {'Content-Type': 'application/x-www-form-urlencoded'}
+]
+
+params = [
+    {},
+    {'username': 'joe', 'password': 'mypass'},
+    {'username': 'joe', 'password': 'mypass', 'email': 'joe@boo.com'},
+    {'message': 'hi there', 'to': 'goo@lala.hu', 'subject': 'Welcome'}
+]
+
+methods = ['POST', 'GET', 'DELETE', 'PUT', 'PATCH', 'OPTIONS']
+
+def fake_data():
+    return ({'method': methods[random.randint(0, len(methods) - 1)],
+          'url': config['base_url'] + urls[random.randint(0, len(urls) - 1)],
+          'header': headers[random.randint(0, len(headers) - 1)],
+          'param': params[random.randint(0, len(params) - 1)]
+        } for x in range(config['requests_per_second'] * config['test_lenght_in_seconds']))
